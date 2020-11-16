b463959b76170127b618f88807d9896e8be0ae04
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-11-16 14:50:59 +0300

    reapo pushed

---
+++ src/app/navbar/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  
+  repos:any;
+  profile:any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
